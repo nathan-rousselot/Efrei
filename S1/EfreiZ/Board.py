3c5467157a8bf844280c57148ec95b185115d0b4
Nathan Rousselot <nathan.rousselot@efrei.net>
2018-11-26 11:58:27 +0100

    Add files via upload

--- S1/EfreiZ/Board.py
+++ S1/EfreiZ/Board.py
@@ -9,7 +9,7 @@ grid = [[0 for i in range(20)] for j in range(20)]
 player = Players(9, 10, grid, True)
 grid[player.x][player.y] = 3
 score = 0
-
+pastpos = [player.x, player.y]
 
 def refresh_game():
     for i in range(20):
@@ -87,6 +87,18 @@ def move(i, j):
         grid[i][j], grid[i][j - 1] = 0, 7
 
 
+def anticheat(pastx, pasty, currx, curry):
+    if sqrt((currx-pastx) ** 2 + (curry-pasty) ** 2) != 1:
+        sys.exit("VAC Banned")
+    count = 0
+    for i in range(20):
+        for j in range(20):
+            if grid[i][j] == 3:
+                count += 1
+    if count > 1:
+        sys.exit("VAC Banned")
+
+
 def gridEdit(i, j, val):
     if 0 > i + val[0] > 19 or 0 > j + val[1] > 19 or grid[i + val[0]][j + val[1]] != 0 or sqrt(val[0]**2 + val[1]**2) != 1 :
         if grid[i + val[0]][j + val[1]] == 7:
@@ -100,9 +112,11 @@ def gridEdit(i, j, val):
         root.mainloop()
         return False
     else:
+        pastpos = [player.x, player.y]
         grid[i][j], grid[i + val[0]][j + val[1]] = 0, 3
         player.x += val[0]
         player.y += val[1]
+        anticheat(pastpos[0], pastpos[1], player.x, player.y)
         return True
 
 
