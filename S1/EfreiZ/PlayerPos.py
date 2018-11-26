89a5f277c3eb4cd301131c0a2d1580649cf8eb27
Nathan Rousselot <nathan.rousselot@efrei.net>
2018-11-26 13:44:08 +0100

    Update PlayerPos.py

--- S1/EfreiZ/PlayerPos.py
+++ S1/EfreiZ/PlayerPos.py
@@ -19,15 +19,8 @@ class Players:
         self.alive = alive
 
     def move_player(self):
-        c = random.randint(0, 3)
-        if c == 0:
-            return [1, 0]
-        elif c == 1:
-            return [0, 1]
-        elif c == 2:
-            return [-1, 0]
-        elif c == 3:
-            return [0, -1]
+        #TODO
+        return None
 
 
 """Uncomment the line you want. Fast == 1000 simulations of your code in a blink of an eye (just kidding, my gross code is slow).
